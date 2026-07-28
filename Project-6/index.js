4d157328ef4e68cebf16d3c9dd08a1069d543f40
amitgautam <amit.gautam@skyestoneai.com>
2026-07-28 19:01:16 +0530

    Validation-2

--- Project-6/index.js
+++ Project-6/index.js
@@ -2,3 +2,32 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("form");
+});
+
+// Handle form submission
+app.post("/submit", (req, res) => {
+    const { name, email, phone, subject, message } = req.body;
+
+    console.log({
+        name,
+        email,
+        phone,
+        subject,
+        message
+    });
+
+    res.render("form", {
+        success: "Your message has been submitted successfully!"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server started successfully on http://localhost:3000");
+});
